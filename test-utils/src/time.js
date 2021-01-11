e8c620e8f52eb9535d3bc797b5df480ebd82bbba
Senshi Onion <onion797jp@gmail.com>
2021-01-11 15:52:40 +0700

    time dependency test ready

--- test-utils/src/time.js
+++ test-utils/src/time.js
@@ -1,7 +1,14 @@
 /*
  * Use code from https://medium.com/edgefund/time-travelling-truffle-tests-f581c1964687
  */
-const SECONDS_IN_DAY = 60 * 60 * 24;
+const SEC = 1;
+const MIN = 60 * SEC;
+const HOUR = 60 * MIN;
+const DAY = 24 * HOUR;
+const SETTLE_GAP = 2 * DAY;
+const NOTICE_GAP = 14 * DAY;
+const YEAR = 365 * DAY;
+
 const helper = require("ganache-time-traveler");
 
 const getLatestTimestamp = async () => {
@@ -13,6 +20,13 @@ const getTimestampPlusDays = async (days) => {
 };
 
 module.exports = {
+  SEC,
+  MIN,
+  HOUR,
+  DAY,
+  SETTLE_GAP,
+  NOTICE_GAP,
+  YEAR,
   advanceTime: helper.advanceTime,
   advanceBlock: helper.advanceBlock,
   advanceBlockAndSetTime: helper.advanceBlockAndSetTime,
--- test/loan-unit.js
+++ test/loan-unit.js
@@ -14,11 +14,17 @@ const {
   printSched,
 } = require('../test-utils/src/helper');
 const {
-  advanceTime,
+  SEC,
+  MIN,
+  HOUR,
+  DAY,
+  SETTLE_GAP,
+  NOTICE_GAP,
+  YEAR,
+  advanceTimeAndBlock,
   takeSnapshot,
   revertToSnapshot,
   getLatestTimestamp,
-  getTimestampPlusDays,
 } = require('../test-utils').time;
 const {emitted, reverted, notEmitted, equal, notEqual, isTrue, ok} = require('../test-utils').assert;
 
@@ -28,34 +34,99 @@ const val = (obj) => {
 };
 
 const getDate = async () => {
-  const currentTime = await Date.now();
+  const currentTime = await getLatestTimestamp();
   return toDate(currentTime);
 };
 
 contract('Loan Unit Tests', async (accounts) => {
   const [owner, alice, bob, carol] = accounts;
 
-  beforeEach(async () => {
-    const snapShot = await takeSnapshot();
-    snapshotId = snapShot['result'];
-  });
-
-  afterEach(async () => {
-    await revertToSnapshot(snapshotId);
-  });
+  let moneyMarket;
+  let fxMarket;
+  let collateral;
+  let loan;
 
   before('deploy Loan', async () => {
+    let time = await getLatestTimestamp();
+    console.log('before    ', toDate(time));
+
     moneyMarket = await MoneyMarket.new();
     fxMarket = await FXMarket.new();
     collateral = await Collateral.new(moneyMarket.address, fxMarket.address);
     loan = await Loan.new(moneyMarket.address, fxMarket.address, collateral.address);
     await collateral.setLoanAddr(loan.address, {from: owner});
     await moneyMarket.setColAddr(collateral.address);
+    console.log();
+    console.log('moneyMarket addr is', moneyMarket.address);
+    console.log('fxMarket    addr is', fxMarket.address);
+    console.log('collateral  addr is', collateral.address);
+    console.log('loan        addr is', loan.address);
+    console.log('owner       addr is', owner);
+    console.log();
+    console.log('alice       addr is', alice);
+    console.log('bob         addr is', bob);
+    console.log('carol       addr is', carol);
+    console.log();
   });
 
   describe('Test Loan', async () => {
-    it('say hello', async () => {
-      console.log('hello');
+    it('Init Collateral with sample data', async () => {
+      sample.Collateral.forEach(async (item, index) => {
+        let res = await collateral.setColBook(...val(item), {
+          from: accounts[index],
+          // value: 0,
+          value: 100000,
+        });
+        await emitted(res, 'SetColBook');
+        // await emitted(res, 'SetColBook', async (ev) => {
+        //   equal(ev.addr, accounts[index]);
+        //   // console.log('ev is', ev);
+        // });
+      });
+    });
+    it('Init with sample FXMarket', async () => {
+      sample.FXMarket.forEach(async (item) => {
+        let res = await fxMarket.setFXBook(...val(item), {from: alice});
+        await emitted(res, 'SetFXBook');
+        // await emitted(res, 'SetFXBook', async (ev) => {
+        //   equal(ev.addr, alice);
+        // });
+      });
+    });
+  });
+
+  describe('Time Dependency Test', async () => {
+    beforeEach(async () => {
+      let time = await getDate();
+      console.log('beforeEach', time);
+
+      const snapShot = await takeSnapshot();
+      snapshotId = snapShot['result'];
+    });
+
+    afterEach(async () => {
+      await revertToSnapshot(snapshotId);
+
+      let time = await getDate();
+      console.log('afterEach ', time);
+    });
+
+    it('One day forward', async () => {
+      await advanceTimeAndBlock(DAY);
+      let time = await getDate();
+      console.log('test01    ', time);
+
+      console.log('hoge');
+    });
+    it('Coupon notice and payment', async () => {
+      await advanceTimeAndBlock(YEAR - NOTICE_GAP);
+      let time = await getDate();
+      console.log('notice    ', time);
+      await advanceTimeAndBlock(NOTICE_GAP);
+      time = await getDate();
+      console.log('payment    ', time);
+
+      console.log('hoge hoge');
     });
   });
 });
