3c208a8654506ddd5a6fa6e94db4927fb44b60e9
Senshi Onion <onion797jp@gmail.com>
2021-01-22 02:36:40 +0700

    commment added for testnet deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -89,18 +89,18 @@ module.exports = function (deployer, network, accounts) {
   };
 
   deployer.then(async () => {
-    const moneyMarket = await deployer.deploy(MoneyMarket);
-    const fxMarket = await deployer.deploy(FXMarket);
-    const collateral = await deployer.deploy(Collateral, moneyMarket.address, fxMarket.address);
-    const loan = await deployer.deploy(Loan, moneyMarket.address, fxMarket.address, collateral.address);
-    await collateral.setLoanAddr(loan.address);
-    await moneyMarket.setColAddr(collateral.address);
+    // const moneyMarket = await deployer.deploy(MoneyMarket);
+    // const fxMarket = await deployer.deploy(FXMarket);
+    // const collateral = await deployer.deploy(Collateral, moneyMarket.address, fxMarket.address);
+    // const loan = await deployer.deploy(Loan, moneyMarket.address, fxMarket.address, collateral.address);
+    // await collateral.setLoanAddr(loan.address);
+    // await moneyMarket.setColAddr(collateral.address);
 
     // Deployed
-    // const moneyMarket = await MoneyMarket.deployed();
-    // const fxMarket = await FXMarket.deployed();
-    // const collateral = await Collateral.deployed();
-    // const loan = await Collateral.deployed();
+    const moneyMarket = await MoneyMarket.deployed();
+    const fxMarket = await FXMarket.deployed();
+    const collateral = await Collateral.deployed();
+    const loan = await Collateral.deployed();
 
     console.log('Network is', network);
     console.log();
@@ -115,49 +115,54 @@ module.exports = function (deployer, network, accounts) {
     console.log('carol       addr is', carol);
     console.log();
 
-    // Init Collateral
-    sample.Collateral.forEach(async (item, index) => {
-      let res = await collateral.setColBook(...val(item), {
-        from: users[index],
-        // value: 0,
-        value: 100000,
-      });
-      await expectEvent(res, 'SetColBook', {addr: users[index]});
-    });
+    /*
+     * NOTE: If you encountera revert with 'Collateral book not set yet',
+     *       please try to init sample data one-by-one block
+     */
 
-    // Init FX
-    sample.FXMarket.forEach(async (item) => {
-      let res = await fxMarket.setFXBook(...val(item), {from: alice});
-      await expectEvent(res, 'SetFXBook', {addr: alice});
-    });
+     // // Init Collateral
+    // await sample.Collateral.forEach(async (item, index) => {
+    //   let res = await collateral.setColBook(...val(item), {
+    //     from: users[index],
+    //     // value: 0,
+    //     value: 100000,
+    //   });
+    //   await expectEvent(res, 'SetColBook', {addr: users[index]});
+    // });
 
-    // Init MoneyMarket
-    {
-      const [item0, item1, item2, item3, item4] = sample.MoneyMarket;
-      let res0 = await moneyMarket.setMoneyMarketBook(...val(item0), {from: alice});
-      let res1 = await moneyMarket.setMoneyMarketBook(...val(item1), {from: alice});
-      let res2 = await moneyMarket.setMoneyMarketBook(...val(item2), {from: bob});
-      let res3 = await moneyMarket.setMoneyMarketBook(...val(item3), {from: carol});
-      let res4 = await moneyMarket.setMoneyMarketBook(...val(item4), {from: alice});
-      await expectEvent(res0, 'SetMoneyMarketBook', {addr: alice});
-      await expectEvent(res1, 'SetMoneyMarketBook', {addr: alice});
-      await expectEvent(res2, 'SetMoneyMarketBook', {addr: bob});
-      await expectEvent(res3, 'SetMoneyMarketBook', {addr: carol});
-      await expectEvent(res4, 'SetMoneyMarketBook', {addr: alice});
-      await printCol(collateral, alice, 'collateral state for alice after setMoneyMarketBook');
-      await printCol(collateral, bob, 'collateral state for bob after setMoneyMarketBook');
-      await printCol(collateral, carol, 'collateral state for carol after setMoneyMarketBook');
-    }
+    // // Init FX
+    // await sample.FXMarket.forEach(async (item) => {
+    //   let res = await fxMarket.setFXBook(...val(item), {from: alice});
+    //   await expectEvent(res, 'SetFXBook', {addr: alice});
+    // });
 
-    // Init FIL custody addr
-    {
-      let res0 = await collateral.registerFILCustodyAddr(web3.utils.asciiToHex('cid_custody_FIL_0'), users[0]);
-      let res1 = await collateral.registerFILCustodyAddr(web3.utils.asciiToHex('cid_custody_FIL_1'), users[1]);
-      let res2 = await collateral.registerFILCustodyAddr(web3.utils.asciiToHex('cid_custody_FIL_2'), users[2]);
-      await expectEvent(res0, 'RegisterFILCustodyAddr', {addr: users[0]});
-      await expectEvent(res1, 'RegisterFILCustodyAddr', {addr: users[1]});
-      await expectEvent(res2, 'RegisterFILCustodyAddr', {addr: users[2]});
-    }
+    // // Init MoneyMarket
+    // {
+    //   const [item0, item1, item2, item3, item4] = sample.MoneyMarket;
+    //   let res0 = await moneyMarket.setMoneyMarketBook(...val(item0), {from: alice});
+    //   let res1 = await moneyMarket.setMoneyMarketBook(...val(item1), {from: alice});
+    //   let res2 = await moneyMarket.setMoneyMarketBook(...val(item2), {from: bob});
+    //   let res3 = await moneyMarket.setMoneyMarketBook(...val(item3), {from: carol});
+    //   let res4 = await moneyMarket.setMoneyMarketBook(...val(item4), {from: alice});
+    //   await expectEvent(res0, 'SetMoneyMarketBook', {addr: alice});
+    //   await expectEvent(res1, 'SetMoneyMarketBook', {addr: alice});
+    //   await expectEvent(res2, 'SetMoneyMarketBook', {addr: bob});
+    //   await expectEvent(res3, 'SetMoneyMarketBook', {addr: carol});
+    //   await expectEvent(res4, 'SetMoneyMarketBook', {addr: alice});
+    //   await printCol(collateral, alice, 'collateral state for alice after setMoneyMarketBook');
+    //   await printCol(collateral, bob, 'collateral state for bob after setMoneyMarketBook');
+    //   await printCol(collateral, carol, 'collateral state for carol after setMoneyMarketBook');
+    // }
+
+    // // Init FIL custody addr
+    // {
+    //   let res0 = await collateral.registerFILCustodyAddr(web3.utils.asciiToHex('cid_custody_FIL_0'), users[0]);
+    //   let res1 = await collateral.registerFILCustodyAddr(web3.utils.asciiToHex('cid_custody_FIL_1'), users[1]);
+    //   let res2 = await collateral.registerFILCustodyAddr(web3.utils.asciiToHex('cid_custody_FIL_2'), users[2]);
+    //   await expectEvent(res0, 'RegisterFILCustodyAddr', {addr: users[0]});
+    //   await expectEvent(res1, 'RegisterFILCustodyAddr', {addr: users[1]});
+    //   await expectEvent(res2, 'RegisterFILCustodyAddr', {addr: users[2]});
+    // }
 
     // FIL Loan Execution
     // {
