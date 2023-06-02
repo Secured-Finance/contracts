3864b9476c06636b41726b6c12a73d5349bea664
Masayuki Osuka <maskore8@gmail.com>
2023-06-02 13:04:28 +0000

    chore: change the init mint value based on the feedback [SF-441]

--- utils/currencies.ts
+++ utils/currencies.ts
@@ -38,7 +38,7 @@ const currencies: Currency[] = [
     autoRollFeeRate: 250,
     circuitBreakerLimitRange: 1000,
     isCollateral: false,
-    args: ['100000000000000000000000000'], // 100,000,000 eFIL
+    args: ['2500000000000000000000000'], // 2,500,000 eFIL
   },
   {
     symbol: 'USDC',
@@ -50,7 +50,7 @@ const currencies: Currency[] = [
     autoRollFeeRate: 250,
     circuitBreakerLimitRange: 1000,
     isCollateral: true,
-    args: ['1000000000000000'], // 1,000,000,000 USDC
+    args: ['10000000000000'], // 10,000,000 USDC
   },
   {
     symbol: 'WBTC',
@@ -62,7 +62,7 @@ const currencies: Currency[] = [
     autoRollFeeRate: 250,
     circuitBreakerLimitRange: 1000,
     isCollateral: false,
-    args: ['2000000000000'], // 20,000 BTC
+    args: ['40000000000'], // 400 BTC
   },
   {
     symbol: 'WETH',
