13b62818b7594f86a6685751f14a77e3ca4278c2
akihiro tanaka <big.a.0816@gmail.com>
2023-08-17 13:44:52 +0900

    perf: optimize the logic [SF-141]

--- contracts/protocol/LendingMarketController.sol
+++ contracts/protocol/LendingMarketController.sol
@@ -195,7 +195,7 @@ contract LendingMarketController is
      * @param _ccys Currency name list in bytes32
      * @return orderBookDetails The array of Detailed information on the order book.
      */
-    function getOrderBookDetails(bytes32[] memory _ccys)
+    function getOrderBookDetails(bytes32[] calldata _ccys)
         external
         view
         override
@@ -437,7 +437,7 @@ contract LendingMarketController is
      * @return activeOrders The array of active orders in the order book
      * @return inactiveOrders The array of inactive orders
      */
-    function getOrders(bytes32[] memory _ccys, address _user)
+    function getOrders(bytes32[] calldata _ccys, address _user)
         external
         view
         override
@@ -468,7 +468,7 @@ contract LendingMarketController is
      * @param _user User's address
      * @return positions The array of active positions
      */
-    function getPositions(bytes32[] memory _ccys, address _user)
+    function getPositions(bytes32[] calldata _ccys, address _user)
         external
         view
         override
@@ -812,7 +812,7 @@ contract LendingMarketController is
      * @param _currencies Currency name list in bytes32
      * @param _maturity The maturity of the selected market
      */
-    function executeItayoseCalls(bytes32[] memory _currencies, uint256 _maturity)
+    function executeItayoseCalls(bytes32[] calldata _currencies, uint256 _maturity)
         external
         override
         nonReentrant
