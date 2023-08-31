698aa70ae3b947605ff8b76e05a14440f24c046f
akihiro tanaka <big.a.0816@gmail.com>
2023-08-31 13:22:40 +0900

    test: update code for testing [SF-723]

--- test/unit/lending-market/circuit-breakers.test.ts
+++ test/unit/lending-market/circuit-breakers.test.ts
@@ -43,10 +43,6 @@ describe.only('LendingMarket - Circuit Breakers', () => {
     [owner, alice, bob, carol, ...signers] = await ethers.getSigners();
     targetCurrency = ethers.utils.formatBytes32String('Test');
 
-    console.log('alice:', alice.address);
-    console.log('bob:', bob.address);
-    console.log('carol:', carol.address);
-
     ({ lendingMarketCaller, lendingMarket, orderActionLogic } =
       await deployContracts(owner, targetCurrency));
   });
@@ -73,8 +69,6 @@ describe.only('LendingMarket - Circuit Breakers', () => {
     side: number,
     unitPrice: number,
   ): Promise<number> => {
-    console.log('createInitialOrders==================== start');
-
     const offsetUnitPrice =
       side === Side.LEND
         ? CIRCUIT_BREAKER_BORROW_THRESHOLD - 1
@@ -99,8 +93,6 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         '100000000000000',
         offsetUnitPrice,
       );
-
-    console.log('createInitialOrders==================== end');
 
     return offsetUnitPrice;
   };
@@ -214,9 +206,7 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         await ethers.provider.send('evm_mine', []);
         await ethers.provider.send('evm_setAutomine', [true]);
 
-        console.log('bobTx.wait()====================');
         await bobTx.wait();
-        console.log('carolTx.wait()====================');
         await carolTx.wait();
 
         await expect(bobTx)
@@ -289,9 +279,7 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         await ethers.provider.send('evm_mine', []);
         await ethers.provider.send('evm_setAutomine', [true]);
 
-        console.log('bobTx.wait()====================');
         await bobTx.wait();
-        console.log('carolTx.wait()====================');
         await carolTx.wait();
 
         await expect(bobTx)
@@ -415,13 +403,9 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         await ethers.provider.send('evm_mine', []);
         await ethers.provider.send('evm_setAutomine', [true]);
 
-        console.log('bobTx.wait()====================');
         await bobTx.wait();
-        console.log('carolTx1.wait()====================');
         await carolTx1.wait();
-        console.log('aliceTx.wait()====================');
         await aliceTx.wait();
-        console.log('carolTx2.wait()====================');
         await carolTx2.wait();
 
         await expect(carolTx1)
@@ -491,9 +475,7 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         await ethers.provider.send('evm_mine', []);
         await ethers.provider.send('evm_setAutomine', [true]);
 
-        console.log('bobTx.wait()====================');
         await bobTx.wait();
-        console.log('carolTx.wait()====================');
         await carolTx.wait();
 
         await expect(bobTx)
@@ -566,9 +548,7 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         await ethers.provider.send('evm_mine', []);
         await ethers.provider.send('evm_setAutomine', [true]);
 
-        console.log('bobTx.wait()====================');
         await bobTx.wait();
-        console.log('carolTx.wait()====================');
         await carolTx.wait();
 
         await expect(bobTx)
@@ -662,9 +642,7 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         await ethers.provider.send('evm_mine', []);
         await ethers.provider.send('evm_setAutomine', [true]);
 
-        console.log('bobTx.wait()====================');
         await bobTx.wait();
-        console.log('carolTx.wait()====================');
         await carolTx.wait();
 
         await expect(bobTx)
@@ -758,9 +736,7 @@ describe.only('LendingMarket - Circuit Breakers', () => {
         await ethers.provider.send('evm_mine', []);
         await ethers.provider.send('evm_setAutomine', [true]);
 
-        console.log('bobTx.wait()====================');
         await bobTx.wait();
-        console.log('carolTx.wait()====================');
         await carolTx.wait();
 
         await expect(bobTx)
